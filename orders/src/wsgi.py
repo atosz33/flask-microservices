511d48a3f2e12232f5712e66343964bdac6e3b23
atosz33 <attilakis33@gmail.com>
2021-05-14 13:45:03 +0200

    fix applicaton object loading

--- orders/src/wsgi.py
+++ orders/src/wsgi.py
@@ -1,7 +1,7 @@
 from app.app import get_app_with_config
 from config import ProductionConfig
 
-if __name__ == '__main__':
-    app, _ = get_app_with_config(ProductionConfig)
+app, mongodb = get_app_with_config(ProductionConfig)
 
+if __name__ == '__main__':
     app.run()
